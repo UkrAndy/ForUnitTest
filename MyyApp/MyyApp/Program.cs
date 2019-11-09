f12046bd9a3b46a54dfe80c62178162134d325f9
UkrAndy <brila_andrij@ukr.net>
2019-11-09 10:52:19 +0300

    my

---
+++ MyyApp/MyyApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyyApp
+{
+    class Program
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
